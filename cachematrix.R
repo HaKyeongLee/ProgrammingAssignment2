3f063950e8defb3ef4b75e2cf0cc9b90c0aaf54e
Ha Kyeong Lee <ha_kyeong.lee@uconn.edu>
2023-10-08 15:25:43 +0900

    Updates with Assignment Two Codes and Comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,54 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) i <<- inverse
+  getinverse <- function() i
+  list(set = set,
+       get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
-
-
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  i <- x$getinverse()
+  if (!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setinverse(i)
+  i
 }
+
+cacheSolve_extra <- function(x, ...) {
+  i <- x$getinverse()
+  if (!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  determinant <- det(data)
+  if (determinant != 0) {
+    i <- solve(data)
+    x$setinverse(i)
+    print("Inverse Matrix:")
+    print(i)
+  } else {
+    cat("Matrix is singular and does not have an inverse.")
+  }
+}
+
+M <- matrix(c(1:4), 2, 2)
+M1 <- makeCacheMatrix(M)
+cacheSolve(M1)
+cacheSolve_extra(M1)
+
+M <- matrix(c(1,2,2,4), 2, 2)
+M2 <- makeCacheMatrix(M)
+cacheSolve(M2)
+cacheSolve_extra(M2)
