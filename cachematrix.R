4847a163fa03bb02c4330ae0e2ce7a20fc8efa4c
Ha Kyeong Lee <145836544+HaKyeongLee@users.noreply.github.com>
2023-10-08 15:42:30 +0900

    Update cachematrix.R
    Saved Extra comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,3 +1,10 @@
+## The first function, `makeCacheMatrix` creates 
+## a special "matrix", which is really a list containing a function to 
+## 1. set the value of the matrix
+## 2.  get the value of the matrix
+## 3.  set the value of the mean
+## 4.  get the value of the mean
+
 makeCacheMatrix <- function(x = matrix()) {
   i <- NULL
   set <- function(y) {
@@ -13,6 +20,14 @@ makeCacheMatrix <- function(x = matrix()) {
        getinverse = getinverse)
 }
 
+## `cacheSolve`: This function computes the inverse of 
+## the special "matrix" returned by `makeCacheMatrix` above. 
+## We assume that the matrix supplied is always invertible. 
+## If the inverse has already been calculated (and the matrix has not changed), 
+## then `cacheSolve` should retrieve the inverse from the cache. 
+## Computing the inverse of a square matrix can be done with the `solve` function in R. 
+## For example, if `X` is a square invertible matrix, then `solve(X)` returns its inverse.
+
 cacheSolve <- function(x, ...) {
   i <- x$getinverse()
   if (!is.null(i)) {
@@ -24,6 +39,11 @@ cacheSolve <- function(x, ...) {
   x$setinverse(i)
   i
 }
+
+## `cacheSolve_extra`: This function does the same as `cache_Solve` 
+## except when the original matrix is singular. 
+## R will print a message--"Matrix is singular and does not have an inverse"
+## --instead of spitting out an error term.
 
 cacheSolve_extra <- function(x, ...) {
   i <- x$getinverse()
